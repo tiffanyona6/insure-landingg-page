b64eb71dbd42e3f633cf089d550c78239844161e
tiffanyona6 <163582444+tiffanyona6@users.noreply.github.com>
2024-04-29 20:19:15 +0200

    actualizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+import '../scss/styles.scss';
+
+const buttonElement = document.getElementById("icon-menu");
+const menuElement = document.getElementById("menu");
+
+const handleMenu = () => {
+    if (menuElement.classList.contains("menu--show")) {
+        menuElement.classList.remove("menu--show");
+        document.body.classList.remove("open");
+    } else {
+        menuElement.classList.add("menu--show");
+        document.body.classList.add("open");
+    }
+};
+buttonElement.addEventListener("click", handleMenu);
